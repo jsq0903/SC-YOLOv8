686d910eada907434f20a0e25bc1109c4be68880
jsq0903 <144525067+jsq0903@users.noreply.github.com>
2024-12-05 15:08:39 +0800

    Update train.py

--- SC-YOLOv8/train.py
+++ SC-YOLOv8/train.py
@@ -6,8 +6,8 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 if __name__ == '__main__':
 
    
-    model = YOLO('yolov8n.pt')                                              # 权重设置
-    model = YOLO('yolov8-our.yaml')                                          # 模型架构
-    results = model.train(data='CR7-DET.yaml', epochs=300, batch size=16)    # 超参数设置
+    model = YOLO('yolov8n.pt')                                               # Load model with pretrained weights
+    model = YOLO('yolov8-our.yaml')                                          # # Alternatively, you can load a specific model.Path:SC-YOLOv8/ultralytics/cfg/models/v8
 
+    results = model.train(data='CR7-DET.yaml', epochs=300, batch size=16)    # Load additional training parameters from default.yaml.Path:SC-YOLOv8/ultralytics/cfg/default.yaml
 
