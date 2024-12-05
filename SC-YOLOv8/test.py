97f5147c28443b3e38505c0f8a1045d10dcba843
jsq0903 <144525067+jsq0903@users.noreply.github.com>
2024-12-05 14:36:22 +0800

    Update test.py

--- SC-YOLOv8/test.py
+++ SC-YOLOv8/test.py
@@ -6,5 +6,5 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 if __name__ == '__main__':
 
     model = YOLO('')                                                      # 训练集上训练好的best.pt 
-    metrics = model.val(split='test',data='lengzha.yaml')  # 在验证集上评估模型
+    metrics = model.val(split='test',data='CR7-DET.yaml')  # 在验证集上评估模型
 
